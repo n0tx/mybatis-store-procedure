f610f8c13ca0f5e422a1d93b27226dcf3af8463c
n0tx <rcandra91@msn.com>
2024-10-16 19:15:53 +0700

    user controller
    mmebuat http api request handler untuk menampilkan daftar users

--- src/main/java/com/riki/mybatis/controller/UserController.java
+++ src/main/java/com/riki/mybatis/controller/UserController.java
@@ -3,11 +3,11 @@ package com.riki.mybatis.controller;
 import com.riki.mybatis.model.User;
 import com.riki.mybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,5 +20,11 @@ public class UserController {
     public ResponseEntity<String> addUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok("User berhasil dibuat");
+    }
+
+    // Menampilkan daftar pengguna
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 }
