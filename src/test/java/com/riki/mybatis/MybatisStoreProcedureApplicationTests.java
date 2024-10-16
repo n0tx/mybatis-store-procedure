fde7893570d1a200ac48c6864d831a3f78e86975
n0tx <rcandra91@msn.com>
2024-10-16 15:55:33 +0700

    Initialize commit Spring Boot MyBatis MySQL Store Procedure

---
+++ src/test/java/com/riki/mybatis/MybatisStoreProcedureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisStoreProcedureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
